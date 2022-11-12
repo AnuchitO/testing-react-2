f5ae33c04ec2948d3289d6c934b363d60c4e3769
AnuchitO <anuchit.prasertsang@gmail.com>
2022-11-12 10:41:11 +0700

    show count

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css'
 import {Hi} from './Hi'
+import {ShowCounter} from './ShowCount'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Hi name={'Nong'} age={10} />
+        {/* <Hi name={'Nong'} age={10} /> */}
         {/* <Hi name="React!!!!" age={12} /> */}
         {/* <Hi name="AnuchiOiiiii" age={32} /> */}
+        <ShowCounter />
       </header>
     </div>
   )
